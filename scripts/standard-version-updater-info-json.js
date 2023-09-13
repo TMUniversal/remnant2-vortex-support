391f3a955df2e6a03ddb1ee6d0c1add178a408f3
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2023-09-13 12:41:36 +0200

    chore: update version bumper script

--- scripts/standard-version-updater-info-json.js
+++ scripts/standard-version-updater-info-json.js
@@ -8,7 +8,7 @@ module.exports = {
       return null;
   },
   writeVersion: function (contents, version) {
-      const versionRegex = /val v = "(\d+\.\d+\.\d+)"/;
+      const versionRegex = /"version": "(\d+\.\d+\.\d+)",/;
       const match = contents.match(versionRegex);
       if (!match) {
           throw new Error("Could not find version in info.json");
